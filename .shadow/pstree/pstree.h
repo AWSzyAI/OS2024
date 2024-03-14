538b81fb38f177aa8ceeff53e7bfc8f5fd5a2938
tracer-nju <tracer@nju.edu.cn>
2024-03-14 16:15:50 +0800

    Linux 240124 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:15:50 up 1 day, 21:36,  1 user,  load average: 1.30, 1.30, 1.21

--- .shadow/pstree/pstree.h
+++ .shadow/pstree/pstree.h
@@ -56,17 +56,8 @@ static inline void printArray(int *arr, int n){
 }
 static inline void PrintTree(psNode *root, int depth){
     if(!root)return;
-    // for(int i=0;i<depth;i++)printf(" ");
-    
-    for(int i=0;i<root->depth;i++){
-        printf("|");
-        for(int i=0;i<depth;i++)printf(" ");    
-    }
-    
-    // printf("|");
-    // for(int i=0;i<depth;i++)printf("-");
-
-    printf("%d %s %d\n", root->pid, root->name, root->depth);
+    for(int i=0;i<depth;i++)printf("  ");
+    printf("%d %s\n", root->pid, root->name);
     PrintTree(root->FirstSon, depth+1);
     PrintTree(root->NextSibling, depth);
 }
