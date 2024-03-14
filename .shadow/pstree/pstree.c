6943be90f50c6ca8e87313169f6a0281c8f47086
tracer-nju <tracer@nju.edu.cn>
2024-03-14 13:03:33 +0800

    Linux 240124 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:03:33 up 1 day, 18:24,  1 user,  load average: 1.61, 1.33, 1.24

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -1,39 +1,5 @@
 #include "pstree.h"
 
-
-void cmd(int argc, char *argv[]) {
-  int opt;
-  int option_processed = 0; // 标志变量
-  while((opt=getopt(argc,argv,"npV"))!=-1){
-    switch (opt)
-    {
-    case 'n':
-      // exe_n(argc, argv);
-      option_processed = 1; // 设置标志变量
-      break;
-    case 'p':
-      // exe_p(argc, argv);
-      option_processed = 1; // 设置标志变量
-      break;
-    case 'V':
-      exe_V(argc, argv);
-      option_processed = 1; // 设置标志变量
-      break;
-    default:
-      printf("<usage> pstree [-npV]\n");
-      option_processed = 1; // 设置标志变量
-      break;
-    }
-  }
-  
-  
-  if(!option_processed &&optind == argc){
-    //getopt()函数的全局变量optind是命令行参数的索引，即argv[]数组的索引
-    printf("[Log] optind = %d No targetPID\n", optind); 
-    cmd_root(argc,argv);
-  }
-}
-
 int main(int argc, char *argv[]) {
   readargs(argc, argv);
   cmd(argc, argv);
