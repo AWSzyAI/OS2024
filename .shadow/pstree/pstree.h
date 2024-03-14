a1e38f45923a2fe81a7058cc8befbcb2914c5da1
tracer-nju <tracer@nju.edu.cn>
2024-03-14 13:08:50 +0800

    Linux 240124 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:08:50 up 1 day, 18:29,  1 user,  load average: 2.28, 1.55, 1.34

--- .shadow/pstree/pstree.h
+++ .shadow/pstree/pstree.h
@@ -239,6 +239,7 @@ static inline void cmd_root(int argc, char *argv[]){
     //构建进程树
     psNode *root = NULL;
     printNode(root);
+    printf("[Log] pids[0] = %d\n", pids[0]);
     root = addNewNode(pids[0], root);
     // for(int i=0;i<cntPIDs;i++)root = addNewNode(pids[i], root);
     //最后输出进程树
