b49544b3a5fd55588d9689de92e24774d7f97269
tracer-nju <tracer@nju.edu.cn>
2024-03-15 07:24:37 +0800

    Linux 240124 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:24:37 up 13:30,  1 user,  load average: 1.87, 0.88, 0.48

--- .shadow/pstree/pstree.h
+++ .shadow/pstree/pstree.h
@@ -401,7 +401,7 @@ static inline void ConstructTree_name(psNode *p, int **pids, int cntPIDs, int pi
                 }
                 temp->NextSibling = q;
             }
-            ConstructTree(q, pids, cntPIDs, pids[i][0]);
+            ConstructTree_name(q, pids, cntPIDs, pids[i][0]);
         }
     }
 
