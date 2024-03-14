bdf367a3b52e7eb3cfa568fdc2e903f09d2ede6e
tracer-nju <tracer@nju.edu.cn>
2024-03-15 07:24:26 +0800

    Linux 240124 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  07:24:26 up 13:30,  1 user,  load average: 1.27, 0.74, 0.43

--- .shadow/pstree/pstree.h
+++ .shadow/pstree/pstree.h
@@ -385,7 +385,27 @@ static inline psNode * addNewNode(int pid, psNode *root){
     return root;
 }
 
+static inline void ConstructTree_name(psNode *p, int **pids, int cntPIDs, int pid){
 
+    for(int i=0;i<cntPIDs;i++){
+        if(pids[i][1] == pid){
+            psNode *q = NewNode(pids[i][0]);
+            q->Parent = p;
+            q->depth = p->depth + 1;
+            if(!p->FirstSon){
+                p->FirstSon = q;
+            }else{
+                psNode *temp = p->FirstSon;
+                while(temp->name < q->name && temp->NextSibling){
+                    temp = temp->NextSibling;
+                }
+                temp->NextSibling = q;
+            }
+            ConstructTree(q, pids, cntPIDs, pids[i][0]);
+        }
+    }
+
+}
 
 static inline void ConstructTree(psNode *p, int **pids, int cntPIDs, int pid){
 
