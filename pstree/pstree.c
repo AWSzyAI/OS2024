729a0e919f42c0029f7e6a0a60626685a0283914
Ziyan Shi -NNU <szy@nnu.edu.cn>
2024-03-17 16:15:08 +0800

    M1: Done

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,6 +1,7 @@
 #include "pstree.h"
 
 
+
 void deleteNode(psNode *root){
     if(!root)return;
     deleteNode(root->FirstSon);
@@ -11,13 +12,7 @@ void deleteNode(psNode *root){
 static inline void cmd_root(int argc, char *argv[]){
     //读取参数，定义root PID
     int rootPID = GetRootPID(argc,argv);
-    // printf("[Log] rootPID = %d\n", rootPID);//[ ] Log系统有待优化
-    
-    //扫描/proc目录，获取所有进程的PID
-    
-    // int *pids = (int*)malloc(1000*sizeof(int));
-    
-    //int pids[1000][2]
+    // get all PIDs
     int **pids = (int**)malloc(1000*sizeof(int*));
     for(int i=0;i<1000;i++){
         pids[i] = (int*)malloc(2*sizeof(int));
@@ -31,10 +26,10 @@ static inline void cmd_root(int argc, char *argv[]){
     // printNode(root);
     // printf("[Log] pids[0] = %d\n", pids[0]);
     root = addNewNode(rootPID, root);
-    ConstructTree(root, pids, cntPIDs, rootPID);
+    ConstructTree_name(root, pids, cntPIDs, rootPID);
     // for(int i=0;i<cntPIDs;i++)root = addNewNode(pids[i], root);
     //最后输出进程树
-    PrintTree(rootPID,root, 10);
+    PrintTree(rootPID,root);
     //释放内存
     free(pids);
     deleteNode(root);
@@ -48,14 +43,13 @@ static inline void exe_n(int argc, char *argv[]){
     int cntPIDs =  getPIDs(pids);
     qsort(pids,cntPIDs,sizeof(int)*2,cmp_pid);// function well
     psNode *root = NULL;
-    root = addNewNode(1, root);
+    root = addNewNode(rootPID, root);
     ConstructTree(root, pids, cntPIDs, rootPID);
-    PrintTree(rootPID,root, 10);
+    PrintTree(rootPID,root);
     free(pids);
     deleteNode(root);
 }
 static inline void exe_V(int argc, char*argv[]){printf("pstree-32/64 (OS2024 - Ziyan Shi) version 0.0.1\nCopyright (C) 2024-2024 NJU and Ziyan Shi\nPSmisc comes with ABSOLUTELY NO WARRANTY.\nThis is free software, and you are welcome to redistribute it under the terms of the GNU General Public License.\nFor more information about these matters, see the files named COPYING\n");}
-
 static inline void exe_p(int argc, char *argv[]){
     int rootPID = GetRootPID(argc,argv);
     int **pids = (int**)malloc(1000*sizeof(int*));
@@ -65,9 +59,9 @@ static inline void exe_p(int argc, char *argv[]){
     int cntPIDs =  getPIDs(pids);
     qsort(pids,cntPIDs,sizeof(int)*2,cmp_pid);// function well
     psNode *root = NULL;
-    root = addNewNode(1, root);
+    root = addNewNode(rootPID, root);
     ConstructTree(root, pids, cntPIDs, rootPID);
-    PrintTree_p(rootPID,root, 10);
+    PrintTree_p(rootPID,root);
     free(pids);
     deleteNode(root);
 }
@@ -96,11 +90,6 @@ static inline void cmd(int argc, char *argv[]) {
         }
     }
 
-    // if(!option_processed &&optind == argc){
-    //     //getopt()函数的全局变量optind是命令行参数的索引，即argv[]数组的索引
-    //     // printf("[Log] optind = %d No targetPID\n", optind); 
-    //     cmd_root(argc,argv);
-    // }
     if(!option_processed){
         cmd_root(argc,argv);
     }
