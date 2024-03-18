546cd803855ab36def93333b6a19a07b6cec53ea
Ziyan Shi <szy@nnu.edu.cn>
2024-03-18 18:45:08 +0800

    M1:allow opt_long

--- pstree/pstree.c
+++ pstree/pstree.c
@@ -1,6 +1,12 @@
 #include "pstree.h"
+#include <getopt.h>
 
-
+struct option long_options[] = {
+    {"version", no_argument, 0, 'V'},
+    {"show-pids",optional_argument,0,'p'},
+    {"numeric-sort",optional_argument,0,'n'},
+    {0, 0, 0, 0}
+};
 
 void deleteNode(psNode *root){
     if(!root)return;
@@ -68,7 +74,7 @@ static inline void exe_p(int argc, char *argv[]){
 static inline void cmd(int argc, char *argv[]) {
     int opt;
     int option_processed = 0; // 标志变量
-    while((opt=getopt(argc,argv,"npV"))!=-1){
+    while((opt=getopt_long(argc,argv,"npV",long_options,NULL))!=-1){
         switch (opt)
         {
         case 'n':
@@ -82,11 +88,15 @@ static inline void cmd(int argc, char *argv[]) {
         case 'V':
         exe_V(argc, argv);
         option_processed = 1; // 设置标志变量
-        break;
+        break;    
         default:
-        printf("<usage> pstree [-npV]\n");
-        option_processed = 1; // 设置标志变量
-        break;
+            if (strcmp(argv[optind - 1], "-V") == 0 || strcmp(argv[optind - 1], "--version") == 0) {
+                    exe_V(argc, argv);
+                } else {
+                    printf("<usage> pstree [-npV]\n");
+                }
+            option_processed = 1; // 设置标志变量
+            break;
         }
     }
 
