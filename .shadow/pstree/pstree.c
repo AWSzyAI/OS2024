9fc6ad0ce966ea9947572a4121b1aa768e77eaa2
tracer-nju <tracer@nju.edu.cn>
2024-03-18 18:21:05 +0800

    Linux newUb 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:05 up  1:16,  1 user,  load average: 0.54, 0.31, 0.16

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -84,7 +84,7 @@ static inline void cmd(int argc, char *argv[]) {
         // option_processed = 1; // 设置标志变量
         // break;
         case '?': // Add a case for '?' to handle unknown options
-            if (strcmp(argv[optind - 1], "-V") == 0 || strcmp(argv[optind - 1], "--version") == 0) {
+            if (strcmp(argv[optind - 1], "--version") == 0) {
                 exe_V(argc, argv);
                 option_processed = 1;
             } else {
