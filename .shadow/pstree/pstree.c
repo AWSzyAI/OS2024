bcd1244ad4405f9f11aa5efcbc582f49f2db3cb8
tracer-nju <tracer@nju.edu.cn>
2024-03-24 15:11:03 +0800

    Linux newUb 6.5.0-25-generic #25~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Feb 20 16:09:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:11:03 up 4 days, 20:07,  2 users,  load average: 0.10, 0.13, 0.16

--- .shadow/pstree/pstree.c
+++ .shadow/pstree/pstree.c
@@ -351,7 +351,7 @@ int getPIDs(int **pids){
         }
         // entry = readdir(dir);
     }
-    
+
     if(dir)closedir(dir);
     return count;
 }
@@ -569,7 +569,7 @@ static inline void cmd_root(int argc, char *argv[]){
         pids[i] = (int*)malloc(2*sizeof(int));
     }
     int cntPIDs =  getPIDs(pids);
-    qsort(pids,cntPIDs,sizeof(int)*2,cmp_pid);// function well
+    qsort(&pids,cntPIDs,sizeof(int)*2,cmp_pid);// function well
     // printf("cntPIDs: %d\n",cntPIDs);
     // printArray(pids,cntPIDs);
     //构建进程树
