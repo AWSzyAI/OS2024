12c060d3bd88c13f26aef3b574281c83efaf9da7
tracer-nju <tracer@nju.edu.cn>
2024-03-27 16:51:26 +0800

    Linux newUb 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:51:26 up 18:25,  2 users,  load average: 0.62, 0.61, 0.61

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -41,8 +41,8 @@ static void draw_tile(int x, int y, int w, int h, uint32_t color) {
 void splash() {
   AM_GPU_CONFIG_T info = {0};
   ioe_read(AM_GPU_CONFIG, &info);
-  w = info.width/2;
-  h = info.height/2;
+  w = info.width;
+  h = info.height;
 
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
@@ -54,6 +54,126 @@ void splash() {
 }
 
 void L0(char *path){
+  AM_GPU_CONFIG_T info = {0};
+  ioe_read(AM_GPU_CONFIG, &info);
+  w = info.width;
+  h = info.height;
+
+  int x = 0;
+  int y = 0;
+  for (; x * SIDE <= w/4; x ++) {
+    for (; y * SIDE <= h/4; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffffff); // white
+      }
+    }
+  }
+  for (; x * SIDE <= w/4; x ++) {
+    for (; y * SIDE <= h/2; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xff0000); // red
+      }
+    }
+  }
+  for (; x * SIDE <= w/4; x ++) {
+    for (; y * SIDE <= h/4*3; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x00ff00); // green
+      }
+    }
+  }
+  for (; x * SIDE <= w/4; x ++) {
+    for (; y * SIDE <= h; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x0000ff); // blue
+      }
+    }
+  }
+  for (; x * SIDE <= w/2; x ++) {
+    for (; y * SIDE <= h/4; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xffff00); // yellow
+      }
+    }
+  }
+  for (; x * SIDE <= w/2; x ++) {
+    for (; y * SIDE <= h/2; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0xff00ff); // purple
+      }
+    }
+  }
+  for (; x * SIDE <= w/2; x ++) {
+    for (; y * SIDE <= h/4*3; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x00ffff); // cyan
+      }
+    }
+  }
+  for (; x * SIDE <= w/2; x ++) {
+    for (; y * SIDE <= h; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x000000); // black
+      }
+    }
+  }
+  for (; x * SIDE <= w/4*3; x ++) {
+    for (; y * SIDE <= h/4; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x7f7f7f); // gray
+      }
+    }
+  }
+  for (; x * SIDE <= w/4*3; x ++) {
+    for (; y * SIDE <= h/2; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x7f0000); // dark red
+      }
+    }
+  }
+  for (; x * SIDE <= w/4*3; x ++) {
+    for (; y * SIDE <= h/4*3; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x007f00); // dark green
+      }
+    }
+  }
+  for (; x * SIDE <= w/4*3; x ++) {
+    for (; y * SIDE <= h; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x00007f); // dark blue
+      }
+    }
+  }
+  for (; x * SIDE <= w; x ++) {
+    for (; y * SIDE <= h/4; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x7f7f00); // dark yellow
+      }
+    }
+  }
+  for (; x * SIDE <= w; x ++) {
+    for (; y * SIDE <= h/2; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x7f007f); // dark purple
+      }
+    }
+  }
+  for (; x * SIDE <= w; x ++) {
+    for (; y * SIDE <= h/4*3; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x007f7f); // dark cyan
+      }
+    }
+  }
+  for (; x * SIDE <= w; x ++) {
+    for (; y * SIDE <= h; y++) {
+      if ((x & 1) ^ (y & 1)) {
+        draw_tile(x * SIDE, y * SIDE, SIDE, SIDE, 0x7f7f7f); // gray
+      }
+    }
+  }
+  
 }
 
 // Operating system is a C program!
