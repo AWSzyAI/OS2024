e9b248e1125fe1e7beea3f8b661910b33c9230bc
tracer-nju <tracer@nju.edu.cn>
2024-03-27 16:47:55 +0800

    Linux newUb 6.5.0-26-generic #26~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Mar 12 10:22:43 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:47:55 up 18:22,  2 users,  load average: 1.38, 0.69, 0.62

--- .shadow/kernel/kernel.c
+++ .shadow/kernel/kernel.c
@@ -41,8 +41,8 @@ static void draw_tile(int x, int y, int w, int h, uint32_t color) {
 void splash() {
   AM_GPU_CONFIG_T info = {0};
   ioe_read(AM_GPU_CONFIG, &info);
-  w = info.width;
-  h = info.height;
+  w = info.width/2;
+  h = info.height/2;
 
   for (int x = 0; x * SIDE <= w; x ++) {
     for (int y = 0; y * SIDE <= h; y++) {
