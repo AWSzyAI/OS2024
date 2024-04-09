9f2f0690683a920befac6e19f05c78c0aac0cdd2
Ziyan Shi <szy@nnu.edu.cn>
2024-04-10 07:18:54 +0800

    M2 puzzle

--- libco/co.c
+++ libco/co.c
@@ -5,10 +5,13 @@ struct co {
 };
 
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
+    //malloc();
     return NULL;
 }
 
 void co_wait(struct co *co) {
+    //当前协程需要等待，直到 co 协程的执行完成才能继续执行 (类似于 pthread_join)
+    free(co);
 }
 
 void co_yield() {
