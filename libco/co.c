56d958f7a8a4a11ebbabffc54d023c6618d97c36
Ziyan Shi <szy@nnu.edu.cn>
2024-04-10 10:19:53 +0800

    start wait

--- libco/co.c
+++ libco/co.c
@@ -1,24 +1,37 @@
 #include "co.h"
 #include <stdlib.h>
 #include <stdint.h>
+#include <ucontext.h>
+    // getcontext(&current->context);
+    // setcontext(&current->context);
+    // makecontext(&current->context, (void (*)(void))func, 1, arg);
+    // swapcontext(&current->context, &co->context);
+
 
 struct context{
-    uint64_t rax;
-    uint64_t rbx;
+    uint64_t rax;//存储函数的返回值
+    
+
     uint64_t rcx;
     uint64_t rdx;
     uint64_t rsi;
     uint64_t rdi;
-    uint64_t rbp;
-    uint64_t rsp;//register stack pointer
     uint64_t r8;
     uint64_t r9;
+
     uint64_t r10;
     uint64_t r11;
+
+    //callee saved/non-volatile registers/call preserved
+    uint64_t rbx;
+    uint64_t rbp;
+    uint64_t rsp;//register stack pointer
     uint64_t r12;
     uint64_t r13;
     uint64_t r14;
     uint64_t r15;
+    
+
     uint64_t rip;
 };
 
@@ -45,13 +58,17 @@ struct co {
 };
 
 struct co* current;
+//协程池
+struct co* co_pool[1024];
+int co_pool_count = 0;
 
+
+//func(arg)被 co_start() 调用，从头开始运行
 struct co *co_start(const char *name, void (*func)(void *), void *arg) {
-    //malloc();
+    //co会被return，所以需要malloc();来保存co的数据。
     struct co *co = malloc(sizeof(struct co));
-    if(co == NULL){
-        return;
-    }
+    assert(co != NULL);
+
     co->name = name;
     co->func = func;
     co->arg = arg;
@@ -59,73 +76,56 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     co->waiterp = NULL;
     co->context.rsp = (uint64_t)co->stack + STACK_SIZE;
     
-    struct co* p = current->waiterp;
-    while(!p){
-        p=p->waiterp;
-    }
-    p->waiterp = co;
-
+    // struct co* p = current->waiterp;
+    // while(!p){
+    //     p=p->waiterp;
+    // }
+    // p->waiterp = co;
     return co;
 }
 
+//当前协程需要等待，直到 co 协程的执行完成才能继续执行 (类似于 pthread_join)
 void co_wait(struct co *co) {
-    //当前协程需要等待，直到 co 协程的执行完成才能继续执行 (类似于 pthread_join)
-
-    free(co);
+    // 执行co的函数
+    co->status=CO_WAITING;
+    co->func(co->arg);
+    co->status=CO_DEAD;
+    free(co);// 每个协程只能被 co_wait 一次
 }
 
 void co_yield() {
-    //当前协程主动让出 CPU，调度执行其他协程
-    // 把寄存器 (包括 Stack Pointer) 都保存下来，然后恢复另一个协程保存的寄存器 (和 Stack Pointer)
-    
-    current->status = CO_RUNNING;
-    // 为每一个协程分配独立的堆栈 (stack)
-    
-
-
     // 保存当前协程的上下文 (context)，包括寄存器 (register) 和堆栈指针 (stack pointer)
+    setcontext(&current->context);
+    current->status = CO_RUNNING;
+    //选择另一个状态为`CO_RUNNING`或`CO_WAITING`的协程
+    struct co *ready_co_list[64];
+    int ready_co_count = 0;
+    for (int i = 0; i < co_pool_count; i++) {
+        if (co_pool[i]->status == CO_RUNNING || co_pool[i]->status == CO_WAITING) {
+            ready_co_list[ready_co_count++] = co_pool[i];
+        }
+    }
+    // 如果没有其他可运行的协程，直接返回
+    if (ready_co_count == 0) {
+        return;
+    }
 
-    // 保存当前协程的栈基址
-    current->context.rbp = (uint64_t)current->stack + STACK_SIZE; //栈底的指针
-    // 保存当前协程的程序计数器
-    asm volatile(
-        "movq %%rsp, %0\n"
-        "movq %%rbp, %1\n"
-        "movq $1f, %2\n"
-        : "=m"(current->context.rsp), "=m"(current->context.rbp), "=m"(current->context.rip)
-    );
-    // 保存当前协程的其他寄存器
-    asm volatile(
-        "movq %%rax, %0\n"
-        "movq %%rbx, %1\n"
-        "movq %%rcx, %2\n"
-        "movq %%rdx, %3\n"
-        "movq %%rsi, %4\n"
-        "movq %%rdi, %5\n"
-        "movq %%r8, %6\n"
-        "movq %%r9, %7\n"
-        "movq %%r10, %8\n"
-        "movq %%r11, %9\n"
-        "movq %%r12, %10\n"
-        "movq %%r13, %11\n"
-        "movq %%r14, %12\n"
-        "movq %%r15, %13\n"
-        : "=m"(current->context.rax), "=m"(current->context.rbx), "=m"(current->context.rcx), "=m"(current->context.rdx), "=m"(current->context.rsi), "=m"(current->context.rdi), "=m"(current->context.r8), "=m"(current->context.r9), "=m"(current->context.r10), "=m"(current->context.r11), "=m"(current->context.r12), "=m"(current->context.r13), "=m"(current->context.r14), "=m"(current->context.r15)
-    );
-    // 保存当前协程的状态
-    current->status = CO_WAITING;
-    // 切换到下一个协程
-    // 从队列中找到下一个协程
-    // 恢复下一个协程的上下文 (context)，包括寄存器 (register) 和堆栈指针 (stack pointer)
-    // 恢复下一个协程的栈指针
-    // 恢复下一个协程的栈基址
-    // 恢复下一个协程的程序计数器
-    // 恢复下一个协程的其他寄存器
-    // 恢复下一个协程的状态
-    // 跳转到下一个协程的程序计数器
-    // 从下一个协程的程序计数器开始执行
-    
-    
-    
+    // 初始化随机数生成器
+    srand(time(NULL));
+
+    // 随机选择一个协程
+    int next_co_index = rand() % ready_co_count;
+
+    // 切换到选定的协程
+    current = ready_co_list[next_co_index];
+    // 恢复选定协程的状态并运行
+    getcontext(&current->context);
+    if (current->status == CO_NEW) {
+        current->status = CO_RUNNING;
+        current->func(current->arg);
+    } else {
+        current->status = CO_RUNNING;
+        setcontext(&current->context);
+    }
 }
 
