964a85ddf4bb5ff5585887c343f86b01175fae1d
Ziyan Shi <szy@nnu.edu.cn>
2024-04-10 08:39:46 +0800

    modify oslab.mk

--- oslabs.mk
+++ oslabs.mk
@@ -1,7 +1,4 @@
 export TOKEN := BRrhv1Be
-export AM_HOME := $(PWD)/abstract-machine
-git remote set-url backup https://github.com/AWSzyAI/OS2024.git
-git remote set-url origin https://git.nju.edu.cn/jyy/os-workbench.git
 
 # ----- DO NOT MODIFY -----
 
