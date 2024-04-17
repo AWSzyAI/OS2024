2227365ebf7e2a3dd3357ccb534474ead69d3894
tracer-nju <tracer@nju.edu.cn>
2024-04-17 13:06:54 +0800

    Linux newUb 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:06:54 up 1 day,  5:37,  1 user,  load average: 0.05, 0.02, 0.00

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -81,6 +81,7 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
 void co_wait(struct co *co) {
     assert(co != NULL);
     debug("co_wait(%s)\n",co->name);
+    co->waiterp=current;
 
     while(co->status!=CO_DEAD){
         // current->status = CO_WAITING;
