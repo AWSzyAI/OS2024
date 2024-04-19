6ea7ac1c7b8108b434c808847965f4683658370e
tracer-nju <tracer@nju.edu.cn>
2024-04-19 11:38:06 +0800

    Linux newUb 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:38:06 up 13:34,  2 users,  load average: 0.58, 0.35, 0.24

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -158,7 +158,7 @@ void co_wait(struct co *co) {
     }
 
     debug_co_stack();
-    debug("free(%s)\n", co->name);    
+    // debug("free(%s)\n", co->name);    
     co->status = CO_DEAD;
     refresh_co_stack();
     // debug("refresh_co_stack()\n");
