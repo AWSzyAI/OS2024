1543a5dc354380a7785dbe791e7f8d4bc8045211
tracer-nju <tracer@nju.edu.cn>
2024-04-21 08:56:45 +0800

    Linux newUb 6.5.0-27-generic #28~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 15 10:51:06 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:56:45 up 2 days, 10:53,  2 users,  load average: 0.28, 0.23, 0.22

--- .shadow/libco/co.c
+++ .shadow/libco/co.c
@@ -130,7 +130,7 @@ struct co *co_start(const char *name, void (*func)(void *), void *arg) {
     
     debug("co(%s) = %p\n",co->name, co); 
     //func(arg)被 co_start() 调用，从头开始运行    
-    makecontext(&co->context, (void (*)(void))wrapper_func,1,co);
+    makecontext(&co->context, (void (*)(void))wrapper_func,1,&co);
     
     co_stack[co_stack_count++] = co;
     debug_co_stack();   
